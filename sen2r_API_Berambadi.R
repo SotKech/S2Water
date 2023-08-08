0de06c2e336e1bfe3aa273581aadd8fae6364f28
SotKech <sot.kechagias@gmail.com>
2023-08-08 15:15:05 +0200

    Create to api - one for each AOI

---
+++ sen2r_API_Berambadi.R
@@ -0,0 +1,93 @@
+#                                                                              #
+#               {}|{}                                                          #
+#               /o|o\                                                          #
+#  ___________:/o|||o\}___________    MGI Internship  | S2Water                #
+#   =|=|=|=|:S|":|||:"|K:|=|=|=|=     Author          : Sotirios Kechagias     #
+#    """"""""""\:|||:/""""""""""      Created         : July 07, 2023          #
+#               \.|./                 Last update     : August 08, 2023        #
+#               /o.o\                 R Version       : 4.3.1                  #
+#              /o.o.o\                LICENSE         : CC BY-NC-SA 4.0        #
+#            #|o.o.o.o|#                                                       #
+#                                                                              #
+
+
+#### Package Import ####
+pkgTest <- function(x){  #     pkgTest is a helper function to load packages and
+                         # install packages only when they are not installed yet
+  if (x %in% rownames(installed.packages()) == FALSE){
+    install.packages(x, dependencies= TRUE)}
+  library(x, character.only = TRUE)}
+neededPackages <- c("raster", "methods", "sf", "stars", "data.table", "XML",
+                    "jsonlite", "geojsonio", "foreach", "parallel", "sen2r",
+                    "doParallel", "httr", "RcppTOML")
+for (package in neededPackages){pkgTest(package)}
+
+
+
+#### Data Preparation ####
+# Create directories
+# setwd("D:/Sotirios_Kechagias/S2Water") # ("C:/Projects/S2Water")
+getwd()                       # check if your working directory is correctly set
+data_path   <- "./Data"
+output_path <- "./Output"
+if (!dir.exists(data_path))   {dir.create(data_path)}
+if (!dir.exists(output_path)) {dir.create(output_path)}
+
+safe_dir <- tempfile(pattern = "sen2r_safe_")  # folder to store downloaded SAFE
+
+
+AOI <- sf::st_read("./Data/Berambadi_AOI.geojson")
+
+list <- sen2r::s2_list(
+  tile = "43PFP",
+  orbit = 062,
+  time_interval = c(as.Date("2023-01-01"), as.Date("2023-08-08")),
+  level = "auto",
+  server = "scihub",
+  service = "apihub",
+  max_cloud = 10,
+  availability = "check",
+  output_type = "deprecated"
+)  ; my_df <- as.data.frame(list) ; write.csv(my_df, "./Berambadi.csv")
+
+sen2r::s2_order(list)
+
+start_time <- Sys.time()
+expo <- sen2r(
+  gui = FALSE,
+  sel_sensor = c("s2a", "s2b"),
+  server = "scihub",
+  step_atmcorr = "auto", # means that L2A is first searched on SciHub: if found,
+                         # it is downloaded, if not, the corresponding Level-1C 
+                         # is downloaded and   sen2cor is used to produce L2
+  # sen2cor_use_dem = F  #                                      may be the issue
+  # sen2cor_gipp = NA,   #             Ground Image Processing Parameters (GIPP)
+  max_cloud_safe = 10,
+  timewindow = c(as.Date("2023-01-01"), as.Date("2023-08-08")),
+  extent = AOI,
+  extent_name = "AOI",
+  s2tiles_selected = c("43PFP"),
+  s2orbits_selected = c("062"),
+  list_prods = c("BOA"),
+  list_rgb = c("RGB432B"),
+  # list_indices = c("NDWI"),
+  index_source = "BOA",
+  mask_type = NA,                                       # because of glint
+  max_mask = 10, 
+  clip_on_extent= TRUE,
+  extent_as_mask= TRUE,
+  # overwrite = TRUE
+  path_l1c = safe_dir,
+  path_l2a = safe_dir,
+  path_out = data_path,
+  thumbnails = FALSE,
+  parallel = 3
+) ; end_time <- Sys.time()
+cat("Runtime: ", round(as.numeric(difftime(end_time,start_time, units = "min")),
+                                                      digits = 3),"m", sep = "")
+
+
+
+
+
+
--- sen2r_API_Lebna.R
+++ sen2r_API_Lebna.R
@@ -4,7 +4,7 @@
 #  ___________:/o|||o\}___________    MGI Internship  | S2Water                #
 #   =|=|=|=|:S|":|||:"|K:|=|=|=|=     Author          : Sotirios Kechagias     #
 #    """"""""""\:|||:/""""""""""      Created         : July 07, 2023          #
-#               \.|./                 Last update     : July 07, 2023          #
+#               \.|./                 Last update     : August 08, 2023        #
 #               /o.o\                 R Version       : 4.3.1                  #
 #              /o.o.o\                LICENSE         : CC BY-NC-SA 4.0        #
 #            #|o.o.o.o|#                                                       #
@@ -12,13 +12,13 @@
 
 
 #### Package Import ####
-pkgTest <- function(x){   #    pkgTest is a helper function to load packages and
-                          # install packages only whenthey are not installed yet
+pkgTest <- function(x){  #     pkgTest is a helper function to load packages and
+  # install packages only when they are not installed yet
   if (x %in% rownames(installed.packages()) == FALSE){
     install.packages(x, dependencies= TRUE)}
   library(x, character.only = TRUE)}
 neededPackages <- c("raster", "methods", "sf", "stars", "data.table", "XML",
-                    "jsonlite", "geojsonio", "foreach", "parallel",
+                    "jsonlite", "geojsonio", "foreach", "parallel", "sen2r",
                     "doParallel", "httr", "RcppTOML")
 for (package in neededPackages){pkgTest(package)}
 
@@ -26,7 +26,7 @@ for (package in neededPackages){pkgTest(package)}
 
 #### Data Preparation ####
 # Create directories
-setwd("C:/Projects/S2Water")
+# setwd("D:/Sotirios_Kechagias/S2Water") # ("C:/Projects/S2Water")
 getwd()                       # check if your working directory is correctly set
 data_path   <- "./Data"
 output_path <- "./Output"
@@ -39,16 +39,16 @@ safe_dir <- tempfile(pattern = "sen2r_safe_")  # folder to store downloaded SAFE
 AOI <- sf::st_read("./Data/Lebna_catchment_boundaries.geojson")
 
 list <- sen2r::s2_list(
-  tile = "32SPF",
+  tile = "32SFP",
   orbit = 122,
-  time_interval = c(as.Date("2017-01-01"), as.Date("2023-07-01")),              # (as.Date("2017-01-01"), as.Date("2023-07-01")
+  time_interval = c(as.Date("2023-01-01"), as.Date("2023-07-01")),
   level = "auto",
   server = "scihub",
   service = "apihub",
   max_cloud = 10,
   availability = "check",
   output_type = "deprecated"
-)  ; my_df <- as.data.frame(list) ; write.csv(my_df, "./outputM.csv")
+)  ; my_df <- as.data.frame(list) ; write.csv(my_df, "./Lebna.csv")
 
 sen2r::s2_order(list)
 
@@ -58,22 +58,22 @@ expo <- sen2r(
   sel_sensor = c("s2a", "s2b"),
   server = "scihub",
   step_atmcorr = "auto", # means that L2A is first searched on SciHub: if found,
-                         # it is downloaded, if not, the corresponding Level-1C 
-                         # is downloaded and   sen2cor is used to produce L2
+  # it is downloaded, if not, the corresponding Level-1C 
+  # is downloaded and   sen2cor is used to produce L2
   # sen2cor_use_dem = F  #                                      may be the issue
   # sen2cor_gipp = NA,   #             Ground Image Processing Parameters (GIPP)
   max_cloud_safe = 10,
-  timewindow = c(as.Date("2017-01-01"), as.Date("2023-07-01")),
+  timewindow = c(as.Date("2023-01-01"), as.Date("2023-07-01")),
   extent = AOI,
   extent_name = "AOI",
   s2tiles_selected = c("32SPF"),
   s2orbits_selected = c("122"),
   list_prods = c("BOA"),
-  # list_rgb = c("RGB432B"),
+  list_rgb = c("RGB432B"),
   # list_indices = c("NDWI"),
   index_source = "BOA",
-  mask_type = "nomask", #because of glint
-  max_mask = 10, 
+  mask_type = NA,                  # because of glint
+  # max_mask = 10,                 #
   clip_on_extent= TRUE,
   extent_as_mask= TRUE,
   # overwrite = TRUE
@@ -84,7 +84,7 @@ expo <- sen2r(
   parallel = 3
 ) ; end_time <- Sys.time()
 cat("Runtime: ", round(as.numeric(difftime(end_time,start_time, units = "min")),
-                                                      digits = 3),"m", sep = "")
+                       digits = 3),"m", sep = "")
 
 
 
